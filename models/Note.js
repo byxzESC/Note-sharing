508624a3fa8fce574304bc1310a034a29d4f79a8
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-02-09 14:40:34 -0800

    model done

---
+++ models/Note.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Note extends Model {}
+
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+    user_id: {
+        type:DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'note',
+  }
+);
+
+module.exports = Note;
