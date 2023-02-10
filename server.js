f26ea78bf08ea756063511c934c6473131367731
Ian Senne <ianssenne@gmail.com>
2023-02-10 10:57:33 -0800

    add basic server and schema file and template env

---
+++ server.js
@@ -0,0 +1,44 @@
+require("dotenv/config");
+
+const express = require("express");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const Store = require("connect-session-sequelize")(session.Store);
+const sessionConfig = {
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	cookie: {
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+	},
+	saveUninitialized: false,
+	store: new Store({
+		db: sequelize,
+	}),
+};
+
+const app = express();
+const port = process.env.PORT;
+app.use((req, res, next) => {
+	try {
+		next();
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session(sessionConfig));
+
+app.use("/", require("./controllers"));
+
+sequelize.sync({ force: false }).then(() => {
+	app.listen(port, () => {
+		console.log(`Server listening on port ${port}`);
+	});
+});
