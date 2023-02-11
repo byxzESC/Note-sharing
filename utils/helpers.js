ea2b9f121efff44cf69030c3cd786315f67d19e4
Ian Senne <ianssenne@gmail.com>
2023-02-11 10:29:06 -0800

    add helpers and renderer partial

---
+++ utils/helpers.js
@@ -0,0 +1,15 @@
+const quillToHtml = require("quill-delta-to-html");
+
+module.exports = {
+  log: console.log,
+  /**
+   *
+   * @param {import("./models/Note")} note
+   */
+  html: (note) => {
+    console.log(note);
+    const delta = JSON.parse(note.content);
+    let renderer = new quillToHtml.QuillDeltaToHtmlConverter(delta);
+    return renderer.convert();
+  },
+};
