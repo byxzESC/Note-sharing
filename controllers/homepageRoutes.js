c1016bacef4726c697d1f220af62edc569cbcf66
Milton Ly <ly.milton@gmail.com>
2023-02-12 23:18:57 -0800

    homepageRoutes code added

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -6,6 +6,63 @@ const withAuth = require('../utils/auth')
 router.get('/', withAuth, (req, res) => {
     Note.findAll({
         where: {user_id: req.session.user_id}
+    },
+    {
+        attributes: ['id', 'title', 'content', 'type', 'user_id'],
+        include: [
+            {
+                model: User,
+                attributes: ['name']
+            },
+            {
+                model: Tag,
+                attributes: ['id', 'color', 'darkColor', 'message', 'filledIn'],
+                include: [{
+                    model: User,
+                    attributes: ['name']
+                }]
+            }
+        ]
+    })
+    .then(noteData => {
+        const notes = noteData.map(note => note.get({ plain: true }))
+        res.render('homepage', { notes, loggedIn: true })
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+router.get('/:id', withAuth, (req, res) => {
+    Note.findOne({
+        where: { id: req.params.id},
+        attributes: ['id', 'title', 'content', 'type', 'user_id'],
+        include: [
+            {
+                model: User,
+                attributes: ['name']
+            },
+            {
+                model: Tag,
+                attributes: ['id', 'color', 'darkColor', 'message', 'filledIn'],
+                include: [{
+                    model: User,
+                    attributes: ['name']
+                }]
+            }
+        ]
+    })
+    .then(noteData => {
+        if (!noteData) {
+            res.status(404).json({ message: 'No note found'})
+            return
+        }
+
+        const note = noteData.get({ plain: true })
+        res.render('homepage', { note, loggedIn: true})
+    })
+    .catch(err => {
+        res.status(500).json(err)
     })
 })
 
