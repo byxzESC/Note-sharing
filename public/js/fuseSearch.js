06e8203420aeb7d72dcca6ef5e3acf119278904c
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-02-13 10:32:37 -0800

    new files

---
+++ public/js/fuseSearch.js
@@ -0,0 +1,51 @@
+const Fuse = require("fuse");
+
+function searchByTag(e) {
+  e.preventDefault();
+
+  const searchInput = document.getElementById("").value;
+  const fuseOptions = {
+    shouldSort: true,
+    id: "userInput",
+    keys: ["message"],
+  };
+
+  fetch("/api/note/all", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then(function (response) {
+    const fuse = new Fuse(response, fuseOptions);
+    // search input should be a string
+    const result = fuse.search(searchInput);
+    // result contain all notes with searched tag
+    // 
+  });
+}
+
+document.querySelector(" ").addEventListener("submit", searchByTag);
+
+// you can access original objects using the item property in each result:
+// console.log(result[0].item);
+// search result would be something like this
+// [
+//   {
+//     "item": {
+//       "title": "One Hundred Years of Solitude",
+//       "author": "Gabriel García Márquez",
+//     },
+//   },
+//   {
+//     "item": {
+//       "title": "The Great Gatsby",
+//       "author": "F. Scott Fitzgerald",
+//     },
+//   },
+//   {
+//     "item": {
+//       "title": "Pride and Prejudice",
+//       "author": "Jane Austen",
+//     },
+//   },
+// ];
