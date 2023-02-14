3c79a507cbd8183b81d490bd275349595aae22f5
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-02-13 22:55:55 -0800

    notes render properly

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,12 @@ const apiRoutes = require("./api");
 const homepageRoutes = require("./homepageRoutes");
 const mainpageRoutes = require('./mainpageRoutes');
 
+// routes list
+    // landing page '/landing'  without Auth direct to here
+    // root route   '/'     with loggedIn all notes show list a titles     
+    // note view    '/internal/doc'
+    // note edit    '/internal/doc/edit'
+    // login/signup 'auth'
 
 router.use("/", mainpageRoutes);
 router.use("/homepage", homepageRoutes);
--- controllers/mainpageRoutes.js
+++ controllers/mainpageRoutes.js
@@ -2,53 +2,82 @@ const router = require("express").Router();
 const { render } = require("ejs");
 const sequelize = require('../config/connection')
 const { User, Tag, Note } = require('../models')
-
-router.get('/', (req, res) => {
-    Note.findAll({
-        attributes: [
-            'id',
-            'title',
-            'content',
-            'type',
-            'user_id'
-        ],
-
-        order: [['note_id', 'DESC']],
-
-        include: [
-            {
-                model: User,
-                attributes: ['name']
-            },
-            {
-                model: Tag,
-                attributes: [
-                    'id',
-                    'color',
-                    'darkColor',
-                    'message',
-                    'filledIn'
-                ],
-                include: [{
+// Kyler_Ortiz17@yahoo.com - 1jwVH5JIkW8kzp6
+router.get('/', async (req, res) => {
+    const notesData = await Note.findAll({
+        //     where: {owner_id: req.session.user_id}
+        // },
+        // {
+          // attributes: ['id', 'title', 'content', 'type', 'owner_id'],
+            attributes: ['title', 'content', 'type'],
+            include: [
+                {
                     model: User,
-                    attributes: ['name']
-                }]
-            }
-        ]
-    })
-    .then(dbNoteData => {
-        const notes = dbNoteData.map(note => note.get({ plain: true }));
-        console.log(req.session);
-        res.render('homepage', {
-            notes,
-            loggedIn: req.session.loggedIn
+                    attributes: ['name'],
+                    as: 'owner',
+                },
+                {
+                    model: Tag,
+                    // attributes: ['id', 'color', 'darkColor', 'message', 'filledIn'],
+                    attributes: ['color', 'darkColor', 'message', 'filledIn'],
+                    // include: [{
+                    //     model: User,
+                    //     attributes: ['name']
+                    // }],
+                    as: 'tags'
+                }
+            ]
         })
-    })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json(err)
-    })
+    const notes = notesData.map(note => note.get({ plain: true }));
+    console.log(notes);
+    res.render('pages/home', { notes } );
 })
+// router.get('/', (req, res) => {
+//     Note.findAll({
+//         attributes: [
+//             'id',
+//             'title',
+//             'content',
+//             'type',
+//             'user_id'
+//         ],
+
+//         order: [['note_id', 'DESC']],
+
+//         include: [
+//             {
+//                 model: User,
+//                 attributes: ['name']
+//             },
+//             {
+//                 model: Tag,
+//                 attributes: [
+//                     'id',
+//                     'color',
+//                     'darkColor',
+//                     'message',
+//                     'filledIn'
+//                 ],
+//                 include: [{
+//                     model: User,
+//                     attributes: ['name']
+//                 }]
+//             }
+//         ]
+//     })
+//     .then(dbNoteData => {
+//         const notes = dbNoteData.map(note => note.get({ plain: true }));
+//         console.log(req.session);
+//         res.render('homepage', {
+//             notes,
+//             loggedIn: req.session.loggedIn
+//         })
+//     })
+//     .catch(err => {
+//         console.log(err)
+//         res.status(500).json(err)
+//     })
+// })
 
 router.get('/note/:id', (req,res) => {
     Note.findOne({
