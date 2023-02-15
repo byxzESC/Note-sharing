9aac00e31f1e6479c0314581959c673590237638
Ian <48780301+IanSSenne@users.noreply.github.com>
2023-02-14 20:16:44 -0800

    Revert "Feature/stressed"

--- controllers/internal/doc.js
+++ controllers/internal/doc.js
@@ -18,20 +18,17 @@ async function userAllowedAccessToDoc(req, res, next) {
 				},
 			],
 		});
-		let allowedDocIds = [];
 		for(let i = 0; i < user.notes.length; i++) {
 			if (user.notes[i].id === parseInt(req.query.id)) {
 				return next();
 			}
-			allowedDocIds.push(user.notes[i].id);
 		}
 		for(let i = 0; i < user.visibleNotes.length; i++) {
 			if (user.visibleNotes[i].id === parseInt(req.query.id)) {
 				return next();
 			}
-			allowedDocIds.push(user.visibleNotes[i].id);
 		}
-		return res.status(403).send("You are not allowed to view this document." + allowedDocIds);
+		return res.status(403).send("You are not allowed to view this document.");
 	} catch (e) {
 		console.log(e);
 		return res.status(500).send("Something went wrong.");
@@ -46,30 +43,8 @@ async function userAllowedToViewDoc(req, res, next) {
 }
 
 router.get("/edit", userAllowedAccessToDoc, async (req, res) => { 
-
-	if (req.query.id === "new") {
-		const doc = await Note.create({
-			title: "Untitled",
-			content: "[]",
-			type: "text",
-			owner_id: req.session.userId,
-		});
-		return res.redirect("/internal/doc/edit?id=" + doc.id);
-	}
-	const doc = await Note.findByPk(req.query.id, {
-		include: [
-			{
-				model: Tag,
-				as:"tags"
-			},
-			{
-				model: User,
-				as: "sharedUsers",
-				attributes: ["id", "email", "name"],
-			}
-		]
-	});
-	res.render("pages/doc/edit", { doc: doc.get({ plain: true }),theme: req.query.theme, editCallback:"cb"});
+	const doc = await Note.findByPk(req.query.id);
+	res.render("pages/doc/edit", { doc,theme: req.query.theme, editCallback:"cb"});
 });
 
 router.get("/", userAllowedAccessToDoc, async (req, res) => { 
@@ -78,10 +53,11 @@ router.get("/", userAllowedAccessToDoc, async (req, res) => {
 			{
 				model: Tag,
 				as: "tags",
+				attributes: ["id", "title", "color", "darkColor"],
 			},
 		],
 	});
-	res.render("pages/doc/view", { doc: doc.get({ plain: true }),theme: req.query.theme });
+	res.render("pages/doc/view", { doc });
 });
 
 module.exports = router;
